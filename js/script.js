de742496736393be76970a667fe7d3ed073bcb7b
Jon Yu <jy300x2@gmail.com>
2018-06-09 17:30:04 -0400

    add random background, new quote every 30 secs, add comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+// This calls the randomBackground function when the show another quote buttons is clicked
+document.getElementById('loadQuote').addEventListener("click", randomBackground, false);
 
 var quotes = [
   {
@@ -29,8 +35,6 @@ var quotes = [
 function getRandomQuote(quotes) {
   // This variable allows the function to use any input array size
   var quotesLength = quotes.length;
-  // Since array indexes start at 0, we just need a random index
-  // from 0 up to including 4
   var randomIndex = Math.floor(Math.random() * quotesLength);
   // This returns the entire quote object
   return quotes[randomIndex];
@@ -41,6 +45,7 @@ function printQuote() {
   var print = '';
   print += '<p class="quote">' + quote.quote + '</p>';
   print += '<p class="source">' + quote.source;
+  // There are two if statements since either one can be included
   if (quote.citation) {
     print += '<span class="citation">' + quote.citation + '</span>';
   }
@@ -48,11 +53,25 @@ function printQuote() {
     print += '<span class="year">' + quote.year + '</span>';
   }
   print += '</p>'
-  return print;
+  // This sets the HTML to the quote
+  document.getElementById('quote-box').innerHTML = print;
 }
 
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", function() {
-  document.getElementById('quote-box').innerHTML = printQuote();
-}, false);
+// Generate a random number for RGB color
+function randomColorNumber() {
+  return Math.floor(Math.random() * 256);
+}
+
+function randomBackground() {
+  // Create an array of three random RGB colors
+  var randomColors = [randomColorNumber(), randomColorNumber(), randomColorNumber()]
+  // Create the RGB color string
+  var randomRgb = "rgb(" + randomColors.join(', ') + ")";
+  // Change the background color to randomRgb
+  document.body.style.backgroundColor = randomRgb;
+}
+
+// Generate a random quote every 30 seconds
+setInterval(function() {
+  printQuote();
+}, 30000);
